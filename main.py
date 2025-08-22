e14951c8435499ecf0195f5ce1e9d8d1673f94a6
nadekoff14 <nadekoff14@icloud.com>
2025-08-23 01:56:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import requests
 import google.generativeai as genai
 import aiohttp
+import feedparser
 from dotenv import load_dotenv
 from openai import OpenAI
 from datetime import datetime, timedelta, time, timezone
@@ -690,6 +691,7 @@ async def on_message(message: discord.Message):
 # ボット起動
 # ---------------------
 bot.run(DISCORD_TOKEN)
+
 
 
 
